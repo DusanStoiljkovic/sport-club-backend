806f1d7ab3c31b5cf725f6d43c8595449abd9fb7
Dusan Stoiljkovic <dusanstoiljkovic02@gmail.com>
2025-06-29 14:10:14 +0200

    first init

---
+++ src/test/java/com/singidunum/sports_club_backend/SportsClubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singidunum.sports_club_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsClubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
