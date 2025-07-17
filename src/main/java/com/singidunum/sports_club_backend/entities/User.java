6804ba4a23ea2b70e32c1bbf7ab947dfd23534b2
Dusan Stoiljkovic <dusanstoiljkovic02@gmail.com>
2025-07-17 20:51:59 +0200

    Jwt service

--- src/main/java/com/singidunum/sports_club_backend/entities/User.java
+++ src/main/java/com/singidunum/sports_club_backend/entities/User.java
@@ -2,13 +2,16 @@ package com.singidunum.sports_club_backend.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -18,8 +21,40 @@ public class User {
     private String lastName;
     @Column(name = "email")
     private String email;
+    @Column(name = "password")
+    private String password;
     @Column(name="contact_number")
     private String contactNumber;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Product> products;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
